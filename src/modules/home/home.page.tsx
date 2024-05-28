705894eb01a32a0763aea0239f83bc2e5893d58d
Galdámez <160098844+galdoway@users.noreply.github.com>
2024-05-27 18:41:15 -0600

    feat: adding react query

--- src/modules/home/home.page.tsx
+++ src/modules/home/home.page.tsx
@@ -1,6 +1,60 @@
+import { useQuery } from "@tanstack/react-query";
 import ImageOverlay from "~/shared/components/image-overlay";
 import LetterPullup from "~/shared/components/letter-pullup";
 import ProductCard from "~/shared/components/product-card";
+
+export interface GetProductsResponse {
+  products: Product[];
+  total: number;
+  skip: number;
+  limit: number;
+}
+
+export interface Product {
+  id: number;
+  title: string;
+  description: string;
+  category: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  tags: string[];
+  brand: string;
+  sku: string;
+  weight: number;
+  dimensions: Dimensions;
+  warrantyInformation: string;
+  shippingInformation: string;
+  availabilityStatus: string;
+  reviews: Review[];
+  returnPolicy: string;
+  minimumOrderQuantity: number;
+  meta: Meta;
+  thumbnail: string;
+  images: string[];
+}
+
+export interface Dimensions {
+  width: number;
+  height: number;
+  depth: number;
+}
+
+export interface Meta {
+  createdAt: Date;
+  updatedAt: Date;
+  barcode: string;
+  qrCode: string;
+}
+
+export interface Review {
+  rating: number;
+  comment: string;
+  date: Date;
+  reviewerName: string;
+  reviewerEmail: string;
+}
 
 function HomePage() {
   return (
